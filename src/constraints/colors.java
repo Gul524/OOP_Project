dce89db6bde11a7d60284725aeb686adfea77d50
Gul524 <gullsuleman524@gmail.com>
2025-02-27 19:08:23 +0500

    LoginUi Complete

---
+++ src/constraints/colors.java
@@ -0,0 +1,13 @@
+package constraints;
+import java.awt.Color;
+
+public class colors {
+    
+    static Color primary = new Color(0, 102, 102);
+    static Color secondary = new Color(255,255,255);
+    static Color textwithPrimaryBG = new Color(255,255,255);
+    static Color textwithSecondaryBG = new Color(0,0,0);
+    static Color white = new Color(255, 255, 255);
+    static Color black = new Color(0, 0, 0);
+    static Color redText = new Color(255,0,0);
+}
